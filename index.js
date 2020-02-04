46e847a242a8688f32d483855a4c008f7c8938a1
Ludvig Hagman <ludhag0119@skynet.halmstad.se>
2020-02-04 09:20:49 +0100

    2020-02-04

--- index.js
+++ index.js
@@ -9,18 +9,51 @@ app.use(express.static(__dirname + "/static"))
 app.get("/", function (req, res) {
     res.redirect("/home");
 });
-app.get("/home", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-})
-app.get("/login", function (req, res) {
-    res.sendFile(__dirname + "/login.html");
-})
-app.get("/register", function (req, res) {
-    res.sendFile(__dirname + "/register.html");
-})
+app.get("/loginToken", function (req, res) {
+    res.redirect("/loginToken");
+});
 
-/*Ger en port*/
-const port = process.env.PORT || 3500
+app.post("/login", function (req, res) {
+
+    //Hämta våra användare från db/fil
+    const users = require("./users");
+
+    const user = users.filter(function (u) {
+        console.log(u);
+        console.log(req.body.email);
+        if (req.body.email === u.email) {
+            return true;
+        }
+    });
+
+    // //Om vi har en och exakt en användare med rätt email
+    // if (user.length === 1) {
+    //     //kolla lösenord
+    //     bcrypt.compare(req.body.password, user[0].password, function (err, success) {
+
+    //         if (success) {
+    //             //res.cookie("auth",{httpOnly:true,sameSite:"strict"});
+    //             const token = jwt.sign({
+    //                 email: user[0].email
+    //             }, secret, {
+    //                 expiresIn: "1m"
+    //             });
+    //             res.cookie("token", token, {
+    //                 httpOnly: true,
+    //                 sameSite: "strict"
+    //             });
+    //             res.send("login success !!! wow")
+    //         } else {
+    //             res.send("wrong password");
+    //         }
+    //     });
+    // } else {
+    //     res.send("no such user");
+    // }
+});
+
+/*Assigns a port*/
+const port = process.env.PORT || 3005
 app.listen(port, function () {
     console.log("port:" + port)
 });
---
+++ users.js
@@ -0,0 +1,13 @@
+module.exports = [{
+        id: 1,
+        email: "bwitter.tokendelivery@gmail.com",
+    },
+    {
+        id: 2,
+        email: "joseph@joestar.com",
+    },
+    {
+        id: 3,
+        email: "speedwagon@speedwagonfoundation.com",
+    }
+];
